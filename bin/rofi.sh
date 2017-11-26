d51fb8804d9763454f83e255aea1efdd771f9615
Danilo de Klerk <danilodeklerk@gmail.com>
2017-11-26 20:53:29 +0100

    Surround args in quotes

--- bin/rofi.sh
+++ bin/rofi.sh
@@ -6,5 +6,5 @@ else
 		-color-urgent "#$(getcolor.sh bg), #$(getcolor.sh 1), #$(getcolor.sh bg), #$(getcolor.sh 1), #$(getcolor.sh bg)" \
 		-color-active "#$(getcolor.sh bg), #$(getcolor.sh 4), #$(getcolor.sh bg), #$(getcolor.sh 4), #$(getcolor.sh bg)" \
 		-color-window "#$(getcolor.sh bg), #$(getcolor.sh fg)" \
-		$@
+		"$@"
 fi
