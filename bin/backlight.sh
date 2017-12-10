128619d049cd684b4a5d5527d9e1714d432eee30
Danilo de Klerk <danilodeklerk@gmail.com>
2017-12-10 15:39:46 +0100

    Add brightness notifications

---
+++ bin/backlight.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Take the first argument
+command=$1
+# Shift the arguments so we can process the rest
+shift
+
+# Use the Master channel
+m=Master
+# Interval is either 1 or whatever is supplied as the second argument
+i=${1:-5}
+
+level() {
+	light -G | awk '{print int(($1 + 2.5) / 5) * 5}'
+}
+
+case $command in
+	-|down) light -U ${i} >/dev/null;;
+	+|up) light -A ${i} >/dev/null;;
+	*) light -S $1 >/dev/null;;
+esac
+
+notify-send "Brightness" -h int:value:`level`
