b94bee1a068568be9121e3719e1eb6974667c0e4
Danilo de Klerk <danilodeklerk@gmail.com>
2018-02-17 16:00:07 +0100

    Add wm.sh

---
+++ bin/wm.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+focused="\ue000" # Filled square
+occupied="\ue001" # Thick border square
+free="\ue130" # Regular square
+
+WM_FIFO=/tmp/wm-fifo
+
+# Create teh fifo, if it doesn't already exist
+[ -p "$WM_FIFO" ] || mkfifo "$WM_FIFO"
+
+bspc subscribe desktop > "$WM_FIFO" &
+
+while read -r line
+do
+	case $line in
+		desktop*)
+
+			wm=""
+			num_mon=$(bspc query -M | wc -l)
+
+			wmstatus=$(bspc wm --get-status)
+			IFS=":"
+			set -- ${wmstatus#?}
+			while [ $# -gt 0 ]
+			do
+				item=$1
+				name=${item#?}
+				case $item in
+					[mM]*)
+						# Monitors
+						case $item in
+							m*)
+								# Monitor
+								# FG=${col7}
+								;;
+							M*)
+								# Focused monitor
+								# FG=${col15}
+								;;
+						esac
+						[ $num_mon -lt 2 ] && shift && continue
+						wm="${wm} ${name} "
+						;;
+					[fFoOuU]*)
+						# Desktops
+						case $item in
+							[FOU]*)
+								icon=${focused}
+								;;
+							[Ff]*)
+								icon=${free}
+								;;
+							[Oo]*)
+								icon=${occupied}
+								;;
+							[Uu]*)
+								# urgent desktop
+								;;
+						esac
+						wm="${wm}${icon}"
+						;;
+					[LTG]*)
+						# Layout, state, and flags
+						# TODO: display these
+						;;
+				esac
+				shift
+			done
+			echo -e "%{c}${wm}" | pop.sh \
+				-a c \
+				-c h \
+				-d 0.5 \
+				-n "wmlistener"
+			;;
+	esac
+done < "$WM_FIFO"
