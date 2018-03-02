3eae73d42c29d2b990cf76892740429f90341730
Danilo de Klerk <danilodeklerk@gmail.com>
2018-03-02 13:51:20 +0100

    Redo colours

--- bin/zathurathemer.sh
+++ bin/zathurathemer.sh
@@ -11,54 +11,37 @@ addregex() {
 	sub+=$(mkregex $1 $2)
 }
 
-#000000
-addregex default-bg                  $(getcolor.sh bg)
-#DDDDDD
-addregex default-fg                  $(getcolor.sh fg)
-
-#000000
-addregex statusbar-bg                $(getcolor.sh bg)
-#FFFFFF
-addregex statusbar-fg                $(getcolor.sh 15)
-
-#131313
-addregex inputbar-bg                 $(getcolor.sh 0) 
-#9FBC00
-addregex inputbar-fg                 $(getcolor.sh 15)
-
-#FF1212
-addregex notification-error-bg       $(getcolor.sh 0) 
-#FFFFFF
-addregex notification-error-fg       $(getcolor.sh 1) 
-
-#F3F000
-addregex notification-warning-bg     $(getcolor.sh 0) 
-#000000
-addregex notification-warning-fg     $(getcolor.sh bg)
-
-#00BC00
-addregex highlight-active-color      $(getcolor.sh 15)
-#9FBC00
-addregex highlight-color             $(getcolor.sh 0) 
-
-#9FBC00
-addregex completion-highlight-bg     $(getcolor.sh 0) 
-#232323
-addregex completion-highlight-fg     $(getcolor.sh 7) 
-
-#232323
-addregex completion-bg               $(getcolor.sh 8) 
-#DDDDDD
-addregex completion-fg               $(getcolor.sh fg)
-
-#FFFFFF
-addregex notification-bg             $(getcolor.sh 0) 
-#000000
-addregex notification-fg             $(getcolor.sh bg)
-
-#FFFFFF
-addregex recolor-darkcolor           $(getcolor.sh fg)
-#000000
-addregex recolor-lightcolor          $(getcolor.sh bg)
+addregex completion-bg            $( getcolor.sh 18 ) #232323
+addregex completion-fg            $( getcolor.sh fg ) #DDDDDD
+addregex completion-group-bg      $( getcolor.sh 0  ) #000000
+addregex completion-group-fg      $( getcolor.sh 7  ) #DEDEDE
+addregex completion-highlight-bg  $( getcolor.sh 2  ) #9FBC00
+addregex completion-highlight-fg  $( getcolor.sh 18 ) #232323
+addregex default-bg               $( getcolor.sh 0  ) #000000
+addregex default-fg               $( getcolor.sh fg ) #DDDDDD
+addregex highlight-active-color   $( getcolor.sh 2  ) #00BC00
+addregex highlight-color          $( getcolor.sh 2  ) #9FBC00
+addregex index-active-bg          $( getcolor.sh 2  ) #9FBC00
+addregex index-active-fg          $( getcolor.sh 18 ) #232323
+addregex index-bg                 $( getcolor.sh 18 ) #232323
+addregex index-fg                 $( getcolor.sh fg ) #DDDDDD
+addregex inputbar-bg              $( getcolor.sh 0  ) #131313
+addregex inputbar-fg              $( getcolor.sh 2  ) #9FBC00
+addregex notification-bg          $( getcolor.sh 15 ) #FFFFFF
+addregex notification-error-bg    $( getcolor.sh 15 ) #FFFFFF
+addregex notification-error-fg    $( getcolor.sh 1  ) #FF1212
+addregex notification-fg          $( getcolor.sh 0  ) #000000
+addregex notification-warning-bg  $( getcolor.sh 15 ) #FFFFFF
+addregex notification-warning-fg  $( getcolor.sh 3  ) #FFF712
+addregex recolor-darkcolor        $( getcolor.sh 15 ) #FFFFFF
+addregex recolor-lightcolor       $( getcolor.sh 0  ) #000000
+addregex render-loading-bg        $( getcolor.sh 15 ) #FFFFFF
+addregex render-loading-fg        $( getcolor.sh 0  ) #000000
+addregex statusbar-bg             $( getcolor.sh 0  ) #000000
+addregex statusbar-fg             $( getcolor.sh 15 ) #FFFFFF
+addregex tabbar-bg                $( getcolor.sh 0  ) #000000
+addregex tabbar-fg                $( getcolor.sh 15 ) #FFFFFF
+addregex tabbar-focus-bg          $( getcolor.sh 0  ) #000000
+addregex tabbar-focus-fg          $( getcolor.sh 2  ) #9FBC00
 
 sed -i -E "$sub" $zathurarc
