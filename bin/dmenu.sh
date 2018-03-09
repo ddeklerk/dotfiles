bb5bf1d7f1c51697faadb129117606080212b01e
Danilo de Klerk <danilodeklerk@gmail.com>
2018-03-09 20:15:28 +0100

    Add dmenu scripts

---
+++ bin/dmenu.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+font="\"Gohu GohuFont:pixelsize=11\""
+
+command=""
+
+case $(basename $0) in
+	dmenu_run*)
+		command="dmenu_run"
+		;;
+	dmenu*)
+		command="dmenu"
+		;;
+esac
+
+if [ -x "$(command -v getcolor.sh)" ]; then
+	options="-nb \\#$(getcolor.sh bg) -nf \\#$(getcolor.sh fg) -sb \\#$(getcolor.sh 4) -sf \\#$(getcolor.sh 0)"
+fi
+
+# Escape $@
+[ ${#} -ne 0 ] && printf -v args "%q " "${@}"
+
+eval "${command} ${options} -fn ${font}" "$args"
